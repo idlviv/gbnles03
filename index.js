e11e632bea058a7d78b9e3a102c7de93d7d34cd1
idlviv <idlviv@rambler.ru>
2017-03-20 20:15:16 +0200

    server works, news not

--- index.js
+++ index.js
@@ -1,31 +1,71 @@
 'use strict';
 window.addEventListener('load', function() {
-  var myDiv = document.createElement('div');
-  var myDiv2 = document.createElement('div');
-  var place = document.querySelector('body');
-  var myInput = document.querySelector('#inp');
-  var myButton = document.querySelector('#btn');
-  place.appendChild(myDiv);
-  place.appendChild(myDiv2);
-  // place.insertBefore(myDiv, myButton);
+  var divTranslateChild = document.createElement('div');
+  var divTranslateChild2 = document.createElement('div');
 
-  myButton.addEventListener('click', function() {
-    var key = myInput.value;
+  var divTranslate = document.querySelector('#div_translate');
+  var inpTranslate = document.querySelector('#inp_translate');
+  inpTranslate.value = 'text'
+  var btnTranslate = document.querySelector('#btn_translate');
+
+  var divNewsChild = document.createElement('div');
+  var divNewsChild2 = document.createElement('div');
+  var divNews = document.querySelector('#div_news');
+  var btnNews = document.querySelector('#btn_news');
+
+  divTranslate.appendChild(divTranslateChild);
+  divTranslate.appendChild(divTranslateChild2);
+  divNews.appendChild(divNewsChild);
+  divNews.appendChild(divNewsChild2);
+  // divTranslate.insertBefore(divTranslateChild, btnTranslate);
+  var q = $('.ipsKursTable_interbank');
+  divNewsChild2.innerText = q;
+  console.log(q);
+
+  // var p = document.querySelectorAll('.ipsKursTable_interbank');
+  // console.log(p);
+
+
+  btnTranslate.addEventListener('click', function() {
+    var key = inpTranslate.value;
     var req = new XMLHttpRequest();
-    // req.open('GET', 'http://localhost:8080');
 
     req.open('GET', 'http://localhost:8080?' +
-      'key=trnsl.1.1.20140416T130443Z.49db75a946e5d9df.baa803157e4482838c0612cb9c5aa513643049a4&lang=en-uk&text='+ key );
+      'task=translate&key=trnsl.1.1.20140416T130443Z.49db75a946e5d9df.baa803157e4482838c0612cb9c5aa513643049a4&lang=en-uk&text='+ key );
     req.send();
     req.addEventListener('readystatechange', function() {
       if (req.readyState === req.DONE) {
         if (req.status === 200) {
           var response = JSON.parse(req.responseText);
-          myDiv.innerText = response.text;
+          divTranslateChild.innerText = response.text;
           console.dir(response);
           var headers = req.getAllResponseHeaders();
-          myDiv2.innerText = headers;
+          divTranslateChild2.innerText = headers;
           console.dir(typeof headers);
+        }
+      }
+    });
+
+  });
+
+  btnNews.addEventListener('click', function() {
+    // var key = inpTranslate.value;
+    var req = new XMLHttpRequest();
+
+    req.open('GET', 'http://localhost:8080?' +
+      'task=news');
+    req.send();
+    req.addEventListener('readystatechange', function() {
+      if (req.readyState === req.DONE) {
+        if (req.status === 200) {
+          // var response = JSON.parse(req.responseText);
+          // divNewsChild.innerText = response;
+          divNewsChild.innerText = req.responseText;
+
+          // console.dir(response);
+          var headers = req.getAllResponseHeaders();
+          divNewsChild2.innerText = headers;
+          // console.dir(typeof headers);
         }
       }
     });
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require("http");
 var urlUtils = require('url');
 var requestUtils = require('request');
+var cheerio = require('cheerio');
 
 // var cors = require('cors');
 // use it before all route definitions
@@ -9,26 +10,59 @@ var requestUtils = require('request');
 function onRequest(request, response) {
   var params = urlUtils.parse(request.url, true);
 
-  params.protocol = 'https:';
+  if (params.query.task === 'translate'){
+    params.protocol = 'https:';
+    delete params.pathname;
+    delete params.host;
+    params.hostname = 'translate.yandex.net/api/v1.5/tr.json/translate';
+    var link = urlUtils.format(params);
 
-  delete params.pathname;
-  delete params.host;
-  params.hostname = 'translate.yandex.net/api/v1.5/tr.json/translate';
+    requestUtils.get(link, function (err, res, body) {
+      if (err) {
+        console.error(err);
+      } else {
+        // console.log(body);
+        response.setHeader('Access-Control-Allow-Origin', 'http://localhost:63342');
+        response.writeHead(200, {'Content-Type': 'Text/plain', 'Cache-Control': 'max-age=31536000'});
+        response.write(body);
+        response.end();
+      }
+    });
 
-  var link = urlUtils.format(params);
+  } else if (params.query.task === 'news') {
 
-  requestUtils.get(link, function (err, res, body) {
-    if (err) {
-      console.error(err);
-    } else {
-      // console.log(body);
-      response.setHeader('Access-Control-Allow-Origin', 'http://localhost:63342');
-      response.writeHead(200, {'Content-Type': 'Text/plain', 'Cache-Control': 'max-age=31536000'});
+    params.protocol = 'https:';
+    delete params.pathname;
+    delete params.host;
+    params.hostname = 'kurs.com.ua/mezhbank';
+    var link = urlUtils.format(params);
 
-      response.write(body);
-      response.end();
-    }
-  });
+    requestUtils.get(link, function (err, res, html) {
+      if (err) {
+        console.error(err);
+      } else {
+        response.setHeader('Access-Control-Allow-Origin', 'http://localhost:63342');
+        response.writeHead(200, {'Content-Type': 'Text/plain'});
+
+        var $ = cheerio.load(html);
+        p = $('.ipsKursTable_interbank');
+
+        // $('.ipsKursTable_interbank').each(function(i, element) {
+        //
+        //   // console.log('i', typeof i.toString());
+        //   // console.log('element', typeof element);
+        //   // response.write(JSON.stringify(element));
+        //   response.write(i.toString());
+        // });
+        // console.log(body);
+
+
+
+        response.end();
+      }
+    });
+  }
+
 }
 
 http.createServer(onRequest).listen(8080);
